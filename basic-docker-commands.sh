7178ac528dc04f1594d8ca9f6b5ab5b6cf33feb5
sam <correo3abdssamad@gmail.com>
2019-02-16 12:27:35 +0100

    add basic docker commands

---
+++ basic-docker-commands.sh
@@ -0,0 +1,20 @@
+## build docker image
+docker build -t image_name path/to/project
+
+## run container
+docker run -p <hostPort>:<containerPort> --name container_name image_name
+
+## Run container in background
+docker run -d -p <hostPort>:<containerPort> --name container_name image_name
+
+## remove all docker unused images
+docker rmi $(docker image ls -q)
+
+## remove all docker unused container
+docker rm $(docker ps -q)
+
+## get docker container ip
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <container_id>
+
+## ssh to docker container
+docker exec -i -t <container_id> /bin/bash
